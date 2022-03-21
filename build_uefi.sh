2b5a6ce382d713c09bf38507b764800d06c191d6
sunflower2333 <sunflower2333@outlook.com>
2022-03-21 13:49:08 +0800

    Initial.
    Change resolution to 1080x2340

--- build_uefi.sh
+++ build_uefi.sh
@@ -3,3 +3,9 @@
 stuart_setup -c Platforms/SurfaceDuoPkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
 stuart_update -c Platforms/SurfaceDuoPkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
 stuart_build -c Platforms/SurfaceDuoPkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
+cp Build/SurfaceDuo-AARCH64/DEBUG_CLANG38/FV/SM8150_EFI.fd .
+
+gzip SM8150_EFI.fd
+
+mv SM8150_EFI.fd.gz ~/mnt/uefi_image.gz
+echo move complete
