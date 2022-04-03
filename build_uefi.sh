e58e3fceb60a34ffea1b249adb92ee9b7bcda207
sunflower2333 <sunflower2333@outlook.com>
2022-04-03 15:43:15 +0800

    Add back Usb power supply.
    (Forgot to uncomment it before.XD)

--- build_uefi.sh
+++ build_uefi.sh
@@ -6,6 +6,5 @@ stuart_build -c Platforms/SurfaceDuoPkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
 cp Build/SurfaceDuo-AARCH64/DEBUG_CLANG38/FV/SM8150_EFI.fd .
 
 gzip SM8150_EFI.fd
-
-mv SM8150_EFI.fd.gz ~/mnt/uefi_image.gz
+mv SM8150_EFI.fd.gz ~/mnt/uefi/uefi_image.gz
 echo move complete
