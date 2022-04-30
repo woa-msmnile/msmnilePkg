8995efdf9f334bed522911005dcd52e909c802af
sunflower2333 <sunflower2333@outlook.com>
2022-04-30 23:42:00 +0800

    Fixed build issues

--- build_uefi.sh
+++ build_uefi.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#stuart_setup -c Platforms/SurfaceDuoPkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
-#stuart_update -c Platforms/SurfaceDuoPkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
+stuart_setup -c Platforms/SurfaceDuoPkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
+stuart_update -c Platforms/SurfaceDuoPkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
 stuart_build -c Platforms/SurfaceDuoPkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
 cp Build/SurfaceDuo-AARCH64/DEBUG_CLANG38/FV/SM8150_EFI.fd .
 
-gzip SM8150_EFI.fd
-mv SM8150_EFI.fd.gz ~/mnt/uefi/uefi_image.gz
-echo move complete
+#gzip SM8150_EFI.fd
+#mv SM8150_EFI.fd.gz ~/mnt/uefi/uefi_image.gz
+#echo move complete
