79665e6c94f331938afb0ded713fb25e65fa2281
Waseem Alkurdi <waseem19992009@outlook.com>
2022-05-25 21:48:05 +0300

    build: Call the PowerShell script `build_releaseinfo.ps1` before starting the build

--- build_uefi.sh
+++ build_uefi.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Stamp build:
+pwsh ./build_releaseinfo.ps1
+# Start the actual build:
 stuart_build -c Platforms/SurfaceDuoPkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
 cp Build/SurfaceDuo-AARCH64/DEBUG_CLANG38/FV/SM8150_EFI.fd .
 
