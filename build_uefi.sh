d826aeec73d8b1b1d81bc5e1b5b5344710b3bc48
Waseem Alkurdi <waseem19992009@outlook.com>
2022-05-26 02:50:06 +0300

    build: In build.sh, create ~/mnt/uefi before attempting to copy SM8150_EFI.fd.gz to it

--- build_uefi.sh
+++ build_uefi.sh
@@ -6,6 +6,7 @@ pwsh ./build_releaseinfo.ps1
 stuart_build -c Platforms/SurfaceDuoPkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
 cp Build/SurfaceDuo-AARCH64/DEBUG_CLANG38/FV/SM8150_EFI.fd .
 
+mkdir -p ~/mnt/uefi
 gzip SM8150_EFI.fd
 mv SM8150_EFI.fd.gz ~/mnt/uefi/uefi_image.gz
 echo move complete
