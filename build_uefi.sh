7f97ddf4ffc814b4f0a5d5008d48e4b894452481
sunflower2333 <54024877+sunflower2333@users.noreply.github.com>
2022-05-29 01:02:56 +0800

    build_uefi.sh: Remove Stamp Build command.
    Will move it to another place.

--- build_uefi.sh
+++ build_uefi.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Stamp build:
-pwsh ./build_releaseinfo.ps1
 # Start the actual build:
 stuart_build -c Platforms/SurfaceDuoPkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
-cp Build/SurfaceDuo-AARCH64/DEBUG_CLANG38/FV/SM8150_EFI.fd .
-
 ./build.sh
