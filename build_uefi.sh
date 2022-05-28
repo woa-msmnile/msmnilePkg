f177c14383c116152ec3f857673dc17c53ff50c2
sunflower2333 <54024877+sunflower2333@users.noreply.github.com>
2022-05-29 01:01:01 +0800

    build_uefi.sh: Use build.sh instead of buildimg.sh

--- build_uefi.sh
+++ build_uefi.sh
@@ -6,9 +6,4 @@ pwsh ./build_releaseinfo.ps1
 stuart_build -c Platforms/SurfaceDuoPkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
 cp Build/SurfaceDuo-AARCH64/DEBUG_CLANG38/FV/SM8150_EFI.fd .
 
-mkdir -p ~/mnt/uefi
-gzip SM8150_EFI.fd
-mv SM8150_EFI.fd.gz ~/mnt/uefi/uefi_image.gz
-echo move complete
-cd ~/mnt/uefi/
-./buildimg.sh
+./build.sh
