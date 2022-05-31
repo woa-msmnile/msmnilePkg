8fd43500ccdb0ffa96fdc317b1551e5e7c97462b
sunflower2333 <54024877+sunflower2333@users.noreply.github.com>
2022-06-01 01:23:43 +0800

    Update build_uefi.sh

--- build_uefi.sh
+++ build_uefi.sh
@@ -2,4 +2,5 @@
 
 # Start the actual build:
 stuart_build -c Platforms/SurfaceDuoPkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
-./build.sh $1
+#Following the major tutorial, ELF should be gen respectively.
+#./build.sh $1
