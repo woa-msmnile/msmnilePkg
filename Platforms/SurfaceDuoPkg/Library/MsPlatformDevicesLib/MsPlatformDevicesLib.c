67afbf7e8a03817db889c3367ddd243fad44eb05
sunflower2333 <sunflower2333@outlook.com>
2022-07-13 19:49:09 +0800

    [Merge from https://github.com/WOA-Project/SurfaceDuoPkg] Platform: Hyp Call to allow MPSS to access shared memory

--- Platforms/SurfaceDuoPkg/Library/MsPlatformDevicesLib/MsPlatformDevicesLib.c
+++ Platforms/SurfaceDuoPkg/Library/MsPlatformDevicesLib/MsPlatformDevicesLib.c
@@ -18,7 +18,114 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #include <Library/UefiLib.h>
 #include <Library/UefiRuntimeServicesTableLib.h>
 
-#include "HypervisorMvCalls.h"
+#include <Library/BaseMemoryLib.h>
+#include <Protocol/EFIScm.h>
+#include <Protocol/scm_sip_interface.h>
+
+EFI_STATUS
+EFIAPI
+RFSProtectSharedArea(UINT64 efsBaseAddr, UINT64 efsBaseSize)
+{
+  EFI_STATUS             Status                       = EFI_SUCCESS;
+  hyp_memprot_ipa_info_t ipaInfo;
+  QCOM_SCM_PROTOCOL     *pQcomScmProtocol             = NULL;
+  UINT32                 dataSize                     = 0;
+  UINT32                 sourceVM                     = AC_VM_HLOS;
+  UINT64                 results[SCM_MAX_NUM_RESULTS] = {0};
+  VOID                  *data                         = NULL;
+
+  // Allow both HLOS (Windows) and MSS (Modem Subsystem) to access the shared memory region
+  // This is needed otherwise the Modem Subsystem will CRASH when attempting to read data
+  hyp_memprot_dstVM_perm_info_t dstVM_perm_info[2] = {
+    {
+      AC_VM_HLOS, 
+      (VM_PERM_R | VM_PERM_W), 
+      (UINT64)NULL, 
+      0
+    },
+    {
+      AC_VM_MSS_MSA, 
+      (VM_PERM_R | VM_PERM_W), 
+      (UINT64)NULL, 
+      0
+    }
+  };
+
+  UINT64                 parameterArray[SCM_MAX_NUM_PARAMETERS] = {0};
+  hyp_memprot_assign_t  *assign = (hyp_memprot_assign_t *)parameterArray;
+
+  Status = gBS->LocateProtocol(
+      &gQcomScmProtocolGuid, 
+      NULL, 
+      (VOID **)&pQcomScmProtocol
+  );
+
+  if (EFI_ERROR(Status)) {
+    return Status;
+  }
+
+  // Fill in the address details
+  ipaInfo.IPAaddr = efsBaseAddr;
+  ipaInfo.IPAsize = efsBaseSize;
+
+  dataSize = sizeof(hyp_memprot_ipa_info_t) + 
+                  sizeof(sourceVM) +
+                  (2 * sizeof(hyp_memprot_dstVM_perm_info_t)) + 
+                  4;
+
+  data = AllocateZeroPool(dataSize);
+  if (data == NULL) {
+    return EFI_OUT_OF_RESOURCES;
+  }
+
+  assign->IPAinfolist = (UINT64)data;
+
+  CopyMem(
+    (VOID *)assign->IPAinfolist, 
+    &ipaInfo, 
+    sizeof(hyp_memprot_ipa_info_t)
+  );
+
+  assign->IPAinfolistsize = sizeof(hyp_memprot_ipa_info_t);
+
+  assign->sourceVMlist =
+      (UINT64)data + 
+      sizeof(hyp_memprot_ipa_info_t);
+
+  CopyMem(
+    (VOID *)assign->sourceVMlist, 
+    &sourceVM, 
+    sizeof(sourceVM)
+  );
+
+  assign->srcVMlistsize = sizeof(sourceVM);
+
+  assign->destVMlist =
+      (UINT64)data + 
+      sizeof(hyp_memprot_ipa_info_t) + 
+      sizeof(sourceVM) + 
+      4;
+
+  CopyMem(
+      (VOID *)assign->destVMlist, 
+      dstVM_perm_info,
+      2 * sizeof(hyp_memprot_dstVM_perm_info_t)
+  );
+
+  assign->destVMlistsize = 2 * sizeof(hyp_memprot_dstVM_perm_info_t);
+  assign->spare          = 0;
+
+  // Send the hypervisor call
+  Status = pQcomScmProtocol->ScmSipSysCall(
+      pQcomScmProtocol, 
+      HYP_MEM_PROTECT_ASSIGN, 
+      HYP_MEM_PROTECT_ASSIGN_PARAM_ID,
+      parameterArray, 
+      results
+  );
+
+  return Status;
+}
 
 /**
 Library function used to provide the platform SD Card device path
@@ -124,38 +231,10 @@ BDS_CONSOLE_CONNECT_ENTRY *EFIAPI GetPlatformConsoleList(VOID)
   gBS->FreePool(pSimpleTextDevicePaths);
   gBS->FreePool(pGraphicsOutputDevicePaths);
 
-  DEBUG((EFI_D_ERROR, "Drumroll...\n"));
-  HypBootInfo* hypBootInfo = GetVmData();
-
-  if (hypBootInfo != NULL) {
-    DEBUG((EFI_D_ERROR, "We got Vm Data!\n"));
-
-    DEBUG((EFI_D_ERROR, "hyp_bootinfo_magic: %d\n", hypBootInfo->hyp_bootinfo_magic));
-    DEBUG((EFI_D_ERROR, "hyp_bootinfo_version: %d\n", hypBootInfo->hyp_bootinfo_version));
-    DEBUG((EFI_D_ERROR, "hyp_bootinfo_size: %d\n", hypBootInfo->hyp_bootinfo_size));
-    DEBUG((EFI_D_ERROR, "num_vms: %d\n", hypBootInfo->num_vms));
-    DEBUG((EFI_D_ERROR, "hlos_vm: %d\n", hypBootInfo->hlos_vm));
-    DEBUG((EFI_D_ERROR, "pipe_id: %d\n", hypBootInfo->pipe_id));
-
-    DEBUG((EFI_D_ERROR, "VM List:\n"));
-
-    for (int i = 0; i < hypBootInfo->num_vms; i++) {
-      DEBUG((EFI_D_ERROR, "vm[%d]: vm_type: %d\n", i, hypBootInfo->vm[i].vm_type));
-      DEBUG((EFI_D_ERROR, "vm[%d]: vm_name: %a\n", i, hypBootInfo->vm[i].vm_name));
-      //DEBUG((EFI_D_ERROR, "vm[%d]: uuid: %a\n", i, hypBootInfo->vm[i].uuid));
-      DEBUG((EFI_D_ERROR, "vm[%d]: dtbo_base: %d\n", i, hypBootInfo->vm[i].info.linux_arm.dtbo_base));
-      DEBUG((EFI_D_ERROR, "vm[%d]: dtbo_size: %d\n", i, hypBootInfo->vm[i].info.linux_arm.dtbo_size));
-
-      for (int j = 0; j < 8; j++) {
-        VmMemRegion region = hypBootInfo->vm[i].ddr_region[j];
-        DEBUG((EFI_D_ERROR, "vm[%d]: ddr_region[%d]: base: %d\n", i, j, region.base));
-        DEBUG((EFI_D_ERROR, "vm[%d]: ddr_region[%d]: size: %d\n", i, j, region.size));
-      }
-    }
-
-  } else {
-    DEBUG((EFI_D_ERROR, "We didn't get vm data... why?\n"));
-  }
+  // Allow MPSS and HLOS to access the allocated RFS Shared Memory Region
+  // Normally this would be done by a driver in Linux
+  // TODO: Move to a better place!
+  RFSProtectSharedArea(0x85D00000, 0x00200000);
 
   return pConsoleConnectEntries;
 }
