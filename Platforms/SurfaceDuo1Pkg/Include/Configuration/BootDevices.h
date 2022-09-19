dbee673e1cc62e5f79fc346cdfaa302e1ea7d819
Gustave Monce <gustave.monce@outlook.com>
2022-09-19 18:57:22 +0200

    Project: General cleanup

--- Platforms/SurfaceDuo1Pkg/Include/Configuration/BootDevices.h
+++ Platforms/SurfaceDuo1Pkg/Include/Configuration/BootDevices.h
@@ -1,10 +1,30 @@
 #ifndef _BOOT_DEVICES_H_
 #define _BOOT_DEVICES_H_
 
-#include <Library/DevicePathLib.h>
 #include <Uefi.h>
 
-#define PLAT_KEYPAD_DEVICE_GUID                                                \
+#include <Protocol/DevicePath.h>
+
+#include <Library/DeviceBootManagerLib.h>
+#include <Library/DevicePathLib.h>
+#include <Library/UefiLib.h>
+
+typedef struct {
+  VENDOR_DEVICE_PATH       VendorDevicePath;
+  EFI_DEVICE_PATH_PROTOCOL EndDevicePath;
+} EFI_KEYPAD_DEVICE_PATH;
+
+typedef struct {
+  VENDOR_DEVICE_PATH       DisplayDevicePath;
+  EFI_DEVICE_PATH_PROTOCOL EndDevicePath;
+} EFI_DISPLAY_DEVICE_PATH;
+
+typedef struct {
+   VENDOR_DEVICE_PATH SdCardDevicePath;
+   EFI_DEVICE_PATH    EndDevicePath;
+} EFI_SDCARD_DEVICE_PATH;
+
+#define EFI_KEYPAD_DEVICE_GUID                                                 \
   {                                                                            \
     0xD7F58A0E, 0xBED2, 0x4B5A,                                                \
     {                                                                          \
@@ -12,34 +32,102 @@
     }                                                                          \
   }
 
-/* DevicePath definition for Button driver */
-#pragma pack(1)
-typedef struct {
-  VENDOR_DEVICE_PATH       VendorDevicePath;
-  EFI_DEVICE_PATH_PROTOCOL End;
-} EFI_KEYPAD_DEVICE_PATH;
-#pragma pack()
+#define EFI_SDCARD_DEVICE_PATH_GUID                                            \
+  {                                                                            \
+    0xD1531D41, 0x3F80, 0x4091,                                                \
+    {                                                                          \
+      0x8D, 0x0A, 0x54, 0x1F, 0x59, 0x23, 0x6D, 0x66                           \
+    }                                                                          \
+  }
 
-EFI_KEYPAD_DEVICE_PATH KeyPadDxeDevicePath = {
+EFI_KEYPAD_DEVICE_PATH KeypadDevicePath =
+{
+  {
     {
-        {
-            HARDWARE_DEVICE_PATH,
-            HW_VENDOR_DP,
-            {
-                (UINT8)(sizeof(VENDOR_DEVICE_PATH)),
-                (UINT8)((sizeof(VENDOR_DEVICE_PATH)) >> 8),
-            },
-        },
-        PLAT_KEYPAD_DEVICE_GUID,
+      HARDWARE_DEVICE_PATH,
+      HW_VENDOR_DP,
+      {
+        (UINT8)(sizeof(VENDOR_DEVICE_PATH)),
+        (UINT8)((sizeof(VENDOR_DEVICE_PATH)) >> 8)
+      }
     },
+    EFI_KEYPAD_DEVICE_GUID
+  },
+  {
+    END_DEVICE_PATH_TYPE,
+    END_ENTIRE_DEVICE_PATH_SUBTYPE,
     {
-        END_DEVICE_PATH_TYPE,
-        END_ENTIRE_DEVICE_PATH_SUBTYPE,
-        {
-            (UINT8)(END_DEVICE_PATH_LENGTH),
-            (UINT8)((END_DEVICE_PATH_LENGTH) >> 8),
-        },
-    },
+      (UINT8)(END_DEVICE_PATH_LENGTH),
+      (UINT8)((END_DEVICE_PATH_LENGTH) >> 8)
+    }
+  }
 };
+
+EFI_DISPLAY_DEVICE_PATH DisplayDevicePath =
+{
+  {
+    {
+      HARDWARE_DEVICE_PATH,
+      HW_VENDOR_DP,
+      {
+        (UINT8)(sizeof(VENDOR_DEVICE_PATH)),
+        (UINT8)((sizeof(VENDOR_DEVICE_PATH)) >> 8)
+      }
+    },
+    EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID
+  },
+  {
+    END_DEVICE_PATH_TYPE,
+    END_ENTIRE_DEVICE_PATH_SUBTYPE,
+    {
+      (UINT8)(END_DEVICE_PATH_LENGTH),
+      (UINT8)((END_DEVICE_PATH_LENGTH) >> 8)
+    }
+  }
+};
+
+EFI_SDCARD_DEVICE_PATH SdcardDevicePath =
+{
+  {
+    {
+      HARDWARE_DEVICE_PATH,
+      HW_VENDOR_DP,
+      {
+        (UINT8)(sizeof(VENDOR_DEVICE_PATH)),
+        (UINT8)((sizeof(VENDOR_DEVICE_PATH)) >> 8)
+      }
+    },
+    EFI_SDCARD_DEVICE_PATH_GUID
+  },
+  {
+    END_DEVICE_PATH_TYPE,
+    END_ENTIRE_DEVICE_PATH_SUBTYPE,
+    {
+      (UINT8)(END_DEVICE_PATH_LENGTH),
+      (UINT8)((END_DEVICE_PATH_LENGTH) >> 8)
+    }
+  }
+};
+
+//
+// Predefined platform default console device path
+//
+BDS_CONSOLE_CONNECT_ENTRY gPlatformConsoles[] =
+{
+  {
+    (EFI_DEVICE_PATH_PROTOCOL *)&KeypadDevicePath,
+    CONSOLE_IN
+  },
+  {
+    (EFI_DEVICE_PATH_PROTOCOL *)&DisplayDevicePath,
+    CONSOLE_OUT | STD_ERROR
+  },
+  {
+    NULL,
+    0
+  }
+};
+
+EFI_DEVICE_PATH_PROTOCOL *gPlatformConInDeviceList[] = {NULL};
 
 #endif
