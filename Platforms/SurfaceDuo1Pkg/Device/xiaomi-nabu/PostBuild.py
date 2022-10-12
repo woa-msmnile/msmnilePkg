357925f1297c88349563d9b2fb516f7c42ad71a4
map220v <map220v300@gmail.com>
2022-10-12 23:46:07 +0300

    nabu: Move dtb to ImageResources folder and update PostBuild.py

--- Platforms/SurfaceDuo1Pkg/Device/xiaomi-nabu/PostBuild.py
+++ Platforms/SurfaceDuo1Pkg/Device/xiaomi-nabu/PostBuild.py
@@ -7,34 +7,39 @@ import os
 outputbin_dir = sdbuild.builder.GetOutputBinDirectory()
 output_dir = sdbuild.builder.GetOutputDirectory()
 device_dir = sdbuild.builder.GetTargetDeviceDirectory()
+root_dir = sdbuild.builder.GetWorkspaceRoot()
 
 bootpayload_path = os.path.join(output_dir, 'bootpayload.bin')
 output_path = os.path.join(output_dir, 'uefi.img')
 fd_path = os.path.join(outputbin_dir, 'FV', 'SM8150_EFI.fd')
-dtb_path = os.path.join(device_dir, 'nabu.dtb')
+bootshim_path = os.path.join(root_dir, 'BootShim', 'BootShim.SimpleInit.bin')
+dtb_path = os.path.join(root_dir, 'ImageResources', 'Nabu', 'dtb')
+ramdisk_path = os.path.join(root_dir, 'ImageResources', 'emptyramdisk')
+kernel_path = os.path.join(root_dir, 'ImageResources', 'dummykernel')
 
 logging.info("Generating bootpayload.bin")
 
-with open(bootpayload_path, 'wb') as f:
+with open(bootpayload_path, 'wb') as bootpayload_file:
     logging.info("Writing UEFI...")
-    with open(fd_path, 'rb') as fd:
-        data = fd.read()
-        data = gzip.compress(data, 9)
-        f.write(data)
-    
-    logging.info("Writing DTB...")
-    with open(dtb_path, 'rb') as dtb:
-        data = dtb.read()
-        f.write(data)
+    with open(bootshim_path, 'rb') as bootshim_file:
+        data = bootshim_file.read()
+        bootpayload_file.write(data)
+    with open(fd_path, 'rb') as fd_file:
+        data = fd_file.read()
+        bootpayload_file.write(data)
+    with open(kernel_path, 'rb') as kernel_file:
+        data = kernel_file.read()
+        bootpayload_file.write(data)
 
 logging.info("Writing uefi.img")
 
 mkbootimg.main([
     "--kernel", bootpayload_path,
-    "--ramdisk", "./ImageResources/emptyramdisk",
+    "--ramdisk", ramdisk_path,
+    "--dtb", dtb_path,
     "-o", output_path,
     "--pagesize", "4096",
-    "--header_version", "0",
+    "--header_version", "2",
     "--cmdline", "",
     "--base", "0x0",
     "--os_version", "11.0.0",
