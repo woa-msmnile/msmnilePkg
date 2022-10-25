475eed9d88a8d5d8619717c67fb7503e9807a0bb
Alula <6276139+alula@users.noreply.github.com>
2022-10-25 18:02:18 +0200

    tp1803: post-rebase fixes

--- Platforms/SurfaceDuo1Pkg/Device/nubia-tp1803/PostBuild.py
+++ Platforms/SurfaceDuo1Pkg/Device/nubia-tp1803/PostBuild.py
@@ -7,18 +7,25 @@ import os
 outputbin_dir = sdbuild.builder.GetOutputBinDirectory()
 output_dir = sdbuild.builder.GetOutputDirectory()
 device_dir = sdbuild.builder.GetTargetDeviceDirectory()
+root_dir = sdbuild.builder.GetWorkspaceRoot()
 
 bootpayload_path = os.path.join(output_dir, 'bootpayload.bin')
 output_path = os.path.join(output_dir, 'uefi.img')
 fd_path = os.path.join(outputbin_dir, 'FV', 'SM8150_EFI.fd')
+bootshim_path = os.path.join(root_dir, 'BootShim', 'BootShim.SimpleInit.bin')
 dtb_path = os.path.join(device_dir, 'tp1803.dtb')
 
 logging.info("Generating bootpayload.bin")
 
 with open(bootpayload_path, 'wb') as f:
     logging.info("Writing UEFI...")
+    data = bytes()
+
+    with open(bootshim_path, 'rb') as bootshim_file:
+        data += bootshim_file.read()
+
     with open(fd_path, 'rb') as fd:
-        data = fd.read()
+        data += fd.read()
         data = gzip.compress(data, 9)
         f.write(data)
     
