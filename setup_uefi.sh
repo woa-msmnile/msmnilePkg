4e5b94c58dcf038a7aace7b0f55827810437dc75
sunflower2333 <sunflower2333@outlook.com>
2022-11-06 13:27:00 +0800

    Fix issues while compiling SimpleInit.

--- setup_uefi.sh
+++ setup_uefi.sh
@@ -4,4 +4,5 @@ stuart_setup -c Platforms/SurfaceDuo1Pkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
 stuart_update -c Platforms/SurfaceDuo1Pkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
 
 echo -e "\e[1;36mBuilding SimpleInit Root\e[0m"
+export CROSS_COMPILE=aarch64-linux-gnu-
 bash scripts/gen-rootfs-source.sh ./ ./build ./root
