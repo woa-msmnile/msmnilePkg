bf89ebc843c76c5a300b0fdc8883a55726e18635
sunflower2333 <sunflower2333@outlook.com>
2023-07-31 12:53:43 +0000

    Sdm845: Add basic support for Sdm845.

---
+++ Platforms/HoyaPkg/PythonLibs/PostBuildHeaderVersion3.py
@@ -0,0 +1,51 @@
+import logging
+import gzip
+import os
+import sys
+import mkbootimg
+
+def makeAndroidImage(outputbin_dir, output_dir, root_dir, device_name, dtb_name) :
+    bootpayload_path = os.path.join(output_dir, 'bootpayload.bin')
+    output_path = os.path.join(output_dir, device_name + '.img')
+    fd_path = os.path.join(outputbin_dir, 'FV', 'SDM845_EFI.fd')
+    bootshim_path = os.path.join(root_dir, 'BootShim', 'BootShim.bin')
+    dtb_path = os.path.join(root_dir, "Platforms", "KailuaPkg", "Device", device_name, 'DeviceTreeBlob', 'Android', 'android-' + dtb_name)
+
+    logging.info("Generating bootpayload.bin")
+
+    '''
+      Payload:
+        | BootShim.bin | SDM845_EFI.fd |
+    '''
+    with open(bootpayload_path, 'wb') as f:
+        logging.info("Writing UEFI...")
+        data = bytes()
+
+        with open(bootshim_path, 'rb') as bootshim_file:
+            data += bootshim_file.read()
+
+        with open(fd_path, 'rb') as fd:
+            data += fd.read()
+            data = gzip.compress(data, 9)
+            f.write(data)
+
+#        logging.info("Writing DTB...")
+#        with open(dtb_path, 'rb') as dtb:
+#            data = dtb.read()
+#            f.write(data)
+
+
+    logging.info("Writing " + device_name + '.img')
+
+    mkbootimg.main([
+        "--kernel", bootpayload_path,
+        "-o", output_path,
+        "--pagesize", "4096",
+        "--header_version", "3",
+        "--cmdline", "",
+        "--base", "0x0",
+        "--os_version", "13.0.0",
+        "--os_patch_level", "2023-04-01"
+    ])
+
+#        "--ramdisk", "./ImageResources/emptyramdisk",
