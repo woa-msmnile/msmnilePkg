f7fd19909429ebfcb19415d4f6eaa8b776571d67
analgay1488 <cho812gm@gmail.com>
2024-05-27 16:17:52 +0300

    Delete Platforms directory

--- Platforms/SurfaceDuo1Pkg/Device/oneplus-guacamoleb/DeviceTreeBlob/Linux/LICENSE.md
+++
@@ -1,4 +0,0 @@
-## License
-Source: https://gitlab.com/sm8150-mainline/linux
-
-License: BSD-3-Clause
--- Platforms/SurfaceDuoFamilyPkg/Include/Library/PlatformPrePiLib.h
+++
@@ -1,7 +0,0 @@
-#ifndef _PLATFORM_PREPI_LIB_H_
-#define _PLATFORM_PREPI_LIB_H_
-
-VOID PlatformInitialize(VOID);
-BOOLEAN IsLinuxBootRequested(VOID);
-
-#endif /* _PLATFORM_UTILS_H_ */
