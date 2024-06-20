5ca727bd0120b97faad95364db66574a0e0de83f
KancyJoe <kancy2333@outlook.com>
2024-06-20 17:03:14 +0000

    8250: Correct ramdisk path which cause android image pack failure.

--- Platforms/KonaPkg/PythonLibs/PostBuild.py
+++ Platforms/KonaPkg/PythonLibs/PostBuild.py
@@ -35,7 +35,7 @@ def makeAndroidImage(outputbin_dir, output_dir, root_dir, device_name, dtb_name)
     mkbootimg.main([
         "--kernel", bootpayload_path,
         "-o", output_path,
-        "--ramdisk", "./ImageResources/ramdisk",
+        "--ramdisk", "./ImageResources/emptyramdisk",
         "--pagesize", "4096",
         "--header_version", "2",
         "--cmdline", "console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xa90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm buildvariant=userdebug",
