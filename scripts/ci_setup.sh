7ed20cb1660be87eaf6efe5475a86077489095fd
KancyJoe <kancy2333@outlook.com>
2024-11-04 07:22:57 +0000

    ci: ci_setup.sh: remove firefox in ubuntu runner before upgrade. saving time is saving life.

--- scripts/ci_setup.sh
+++ scripts/ci_setup.sh
@@ -6,6 +6,7 @@ echo CI Environment Setup.
 #sed -i 's/security.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list
 
 # Install Dependence
+apt -y autoremove firefox # uninstall firefox before upgradeing.
 apt update && apt -y upgrade
 apt -y install sudo python3-venv pip git build-essential nuget build-essential uuid-dev iasl nasm gcc-aarch64-linux-gnu python3.10 python3-distutils python3-git python3-pip gettext locales gnupg ca-certificates python3-venv git git-core clang llvm curl mono-devel lld
 ./build_setup.sh
